4a5d849f098adf06fd7ea15ac3249996b7bb0333
jiangzhichao <jiangzhichao@yryz.com>
2017-05-12 18:08:32 +0800

    es

---
+++ src/main/java/com/es/common/EsClient.java
@@ -0,0 +1,18 @@
+package com.es.common;
+
+import java.net.InetAddress;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+public class EsClient {
+	
+	
+	public Client getClient(){
+		new TransportClient();
+		Client client = TransportClient.builder().build()
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+	}
+	
+}
